44fce0ac45ecb7587b006acbcd781f4a0c6d7eee
jcj0002 <jcj0002@gmail.com>
2018-07-25 16:39:19 -0400

    some user and skill seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+
+jane_smith = User.create!(
+    email: 'jane.smith@happyplace.com',
+    password: 'janes',
+    password_confirmation: 'janes'
+)
+
+john_smith = User.create!(
+    email: 'john.smith@happyplace.com',
+    password: 'johns',
+    password_confirmation: 'johns'
+)
+
+jane_smith.skills.create!(
+    title: 'Photographer',
+    description: 'Technical artist who use a camera to capture various subjects.',
+    rating: 5
+)
+
+john_smith.skills.create!(
+    title: 'Real Estate',
+    description: 'Technical artist who use a camera to capture various subjects.',
+    rating: 3
+)

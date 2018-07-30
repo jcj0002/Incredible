f20ebd7c8019f61c656268264c455669c948f94a
jcj0002 <jcj0002@gmail.com>
2018-07-30 10:21:45 -0400

    added secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,3 @@
 Devise.setup do |config|
-    config.secret_key = 
+    config.secret_key = '22c7a8b89447ce048c40a5cbe934200be2c20322cf84f51d388760ee4ba10a7ece395f3f4bebfb4784a6f86297c367c87c15bc65e4a1fe7960684c498cb3c281'
   end
